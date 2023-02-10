33ab6ff30a1f2cb0eee06200fba08831de7fd883
Md Asif Alam <54733354+measifalam@users.noreply.github.com>
2023-02-11 02:18:57 +0530

    Add files via upload

---
+++ 2.1 LinearSearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnDSAlgorithms
+{
+    class Search
+    {
+        public int linearsearch(int[] A, int n, int key)
+        {
+            int index = 0;
+            while (index < n)
+            {
+                if (A[index] == key)
+                    return index;
+                index = index + 1;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Search s = new Search();
+            int[] A = { 84, 21, 47, 96, 15 };
+            int found = s.linearsearch(A, 5, 100);
+            Console.WriteLine("Result: " + found);
+            Console.ReadKey();
+        }
+    }
+}
