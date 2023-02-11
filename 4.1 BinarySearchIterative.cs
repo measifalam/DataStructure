2984f7749d2887e698e92cb725c0887546080f18
Md Asif Alam <54733354+measifalam@users.noreply.github.com>
2023-02-11 13:32:21 +0530

    Add files via upload

---
+++ 4.1 BinarySearchIterative.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnDSAlgorithms
+{
+    class Search
+    {
+        public int binarysearch(int[] A, int n, int key)
+        {
+            int l = 0;
+            int r = n - 1;
+            while (l <= r)
+            {
+                int mid = (l + r) / 2;
+                if (key == A[mid])
+                    return mid;
+                else if (key < A[mid])
+                    r = mid - 1;
+                else if (key > A[mid])
+                    l = mid + 1;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Search s = new Search();
+            int[] A = { 15, 21, 47, 84, 96 };
+            int found = s.binarysearch(A, 5, 96);
+            //int found = s.binarysearch(A, 5, 21);
+            //int found = s.binarysearch(A, 5, 100);
+            Console.WriteLine("Result: " + found);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6.1 BinarySearchRecursive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnDSAlgorithms
+{
+    class Search
+    {
+        public int binarysearch(int[] A, int key, int l, int r)
+        {
+            if (l > r)
+                return -1;
+            else
+            {
+                int mid = (l + r) / 2;
+                if (key == A[mid])
+                    return mid;
+                else if (key < A[mid])
+                    return binarysearch(A, key, l, mid - 1);
+                else if (key > A[mid])
+                    return binarysearch(A, key, mid + 1, r);
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Search s = new Search();
+            int[] A = { 15, 21, 47, 84, 96 };
+            int found = s.binarysearch(A, 96, 0, 4);
+            //int found = s.binarysearch(A, 21, 0, 4);
+            //int found = s.binarysearch(A, 100, 0, 4);
+            Console.WriteLine("Result: " + found);
+            Console.ReadKey();
+        }
+    }
+}
