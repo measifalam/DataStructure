4ad54691a99955d6e774e20b2ddf3701cab2ada9
Md Asif Alam <54733354+measifalam@users.noreply.github.com>
2023-02-23 04:56:59 +0530

    Add files via upload

---
+++ 6.1 LinkedList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnDSAlgorithms
+{
+    public class Node
+    {
+        public int element;
+        public Node next;
+        public Node(int e, Node n)
+        {
+            element = e;
+            next = n;
+        }
+    }
+
+    class LinkedList
+    {
+        private Node head;
+        private Node tail;
+        private int size;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void addLast(int e)
+        {
+            Node newest = new Node(e, null);
+            if (isEmpty())
+                head = newest;
+            else
+                tail.next = newest;
+            tail = newest;
+            size = size + 1;
+        }
+
+        public void display()
+        {
+            Node p = head;
+            while (p != null)
+            {
+                Console.Write(p.element + " --> ");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            LinkedList l = new LinkedList();
+            l.addLast(7);
+            l.addLast(4);
+            l.addLast(12);
+            l.display();
+            Console.WriteLine("Size: " + l.length());
+            l.addLast(8);
+            l.addLast(3);
+            l.display();
+            Console.WriteLine("Size: " + l.length());
+            Console.ReadKey();
+        }
+    }
+}
